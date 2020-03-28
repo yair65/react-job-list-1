6e1d890aab0d7e0dbd6f9da6efef7367263c0ecf
yair65 <yairfeldman65@gmail.com>
2020-03-28 23:42:09 +0300

    commit no 01 from home station

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component{
+  state = {
+    characters: [
+      // {
+      //   name: 'Charlie',
+      //   job: 'Janitor',
+      // },
+      // {
+      //   name: 'Mac',
+      //   job: 'Bouncer',
+      // },
+      // {
+      //   name: 'Dee',
+      //   job: 'Aspring actress',
+      // },
+      // {
+      //   name: 'Dennis',
+      //   job: 'Bartender',
+      // },
+    ],
+  };
+
+  removeCharacter = index => {
+    const {characters} = this.state;
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+
+    render(){
+      return(
+        <div className="container">
+          <Table characterData = {this.state.characters} removeCharacter = {this.removeCharacter} />
+          <Form />
+        </div>
+      );
+    }
+  };
+
+  export default App;
